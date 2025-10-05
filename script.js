4d495cada7a53e78c0c1a9958f475d9ab6abbcb6
supreme-majesty <reborn.websoft@gmail.com>
2025-10-05 20:53:24 +0000

    Refactor index.html to comment out script.js and include while.js; add while loop examples in while.js

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-let myArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// Loops
+// For Loop, While Loop, Do-While Loop, For-In Loop
+
+let myArray = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
+
+for (let i = 0; i < myArray.length; i++) {
+  console.log(myArray[i]);
+}
 
 // 2D Array
-let myArr2D = [
+let my2DArray = [
   [1, 2, 3],
   [4, 5, 6],
   [7, 8, 9],
 ];
- 
-// 3D Array
-let myArr3D = [
+
+for (let i = 0; i < my2DArray.length; i++) {
+  for (let j = 0; j < my2DArray[i].length; j++) {
+    console.log(my2DArray[i][j]);
+  }
+}
+
+// Asigmnment for 3D Array
+let my3DArray = [
   [
     [1, 2],
     [3, 4],
@@ -17,11 +30,8 @@ let myArr3D = [
     [5, 6],
     [7, 8],
   ],
+  [
+    [9, 10],
+    [11, 12],
+  ],
 ];
-
-console.log(myArr3D[1][1][1]);
-
-// console.log(myArr);
-// console.log(myArr2D);
-// console.log(myArr3D);
- 
